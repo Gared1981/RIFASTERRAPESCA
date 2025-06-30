69f3a793fd997fd4a72a8d0d17a4ad27f081e54f
Gared1981 <armeentac@gmail.com>
2025-06-30 12:03:26 -0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Bolt parte desde la raíz
+  root: '.', // asegúrate que index.html esté en la raíz
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html') // 👈 evita el error de entrada
+    }
   }
 })
